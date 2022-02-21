9ba774553ea611a748784833945a826dfe509b1c
bdushimi <lespharaons@hotmail.com>
2022-02-22 01:35:50 +0200

    Adds customer get function

--- gss/customer.js
+++ gss/customer.js
@@ -26,3 +26,21 @@ module.exports.create = async (event) => {
         return sendResponse(500, { message: "Could not create Customer" });
     }
 }
+
+module.export.get = async (event) => {
+    const customer_id = event.pathParameters.customer_id;
+    try {
+        const TableName = process.env.DYNAMODB_TABLE;
+        const params = {
+            TableName,
+            Key: {
+                customer_id
+            }
+        };
+
+        const data = await dynamodb.get(params).promise();
+        return sendResponse(200, { message : 'Customer retrieved successfully', data });
+    } catch (error) {
+        return sendResponse(500, { message: "Could not get Customer" });
+    }
+}
