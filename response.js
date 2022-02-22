3b601adfbcecc02e651e58706f137e665d074345
bdushimi <lespharaons@hotmail.com>
2022-02-22 11:11:58 +0200

    Adds Response Get function

--- response.js
+++ response.js
@@ -15,7 +15,7 @@ module.exports.create = async (event) => {
             TableName,
             Item: {
                 "pk": 'RESPONSE#' + response_id,
-                "sk": 'RESPONSE#' + response_id,
+                "sk": 'RESPONSE#' + survey_id,
                 "response_data": response_data,
             },
             // insert the data if the partition key is not found
@@ -30,7 +30,21 @@ module.exports.create = async (event) => {
     }
 }
 
-module.exports.get = async (event) => { 
-    const customer_id = event.pathParameters.id;
-    
+module.exports.get = async (event) => {
+    const { id } = event.pathParameters;
+    const TableName = process.env.DYNAMODB_TABLE;
+    const params = {
+        TableName,
+        KeyConditionExpression: 'pk = :id',
+        ExpressionAttributeValues: {
+            ':id': 'RESPONSE#' + id
+        }
+    }
+
+    const data = await dynamodb.query(params).promise();
+    if (data.Count > 0) {
+        return sendResponse(200, { message: 'Response has been retrieved successfully', data: data.Items[0] });
+    } else {
+        return sendResponse(404, { message: 'Response not found' });
+    }
 }
