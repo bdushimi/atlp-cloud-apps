dbc2a3d8c05a3be1a586e15a334864bb037bd053
bdushimi <lespharaons@hotmail.com>
2022-03-15 16:14:09 +0200

    Removes duplicate return statements

--- otp-manager/OTPController.js
+++ otp-manager/OTPController.js
@@ -5,10 +5,5 @@ export async function generate(event) {
 
   const { phoneNumber } = JSON.parse(event.body);
 
-  return {
-    statusCode: 200,
-    body : phoneNumber
-  };
-
   return sendResponse(200, { message: 'Phone Number has been retrieved successfully', phoneNumber });
 }
