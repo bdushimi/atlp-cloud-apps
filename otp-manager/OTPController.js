0cf497f1079d71e5f761684b02aa30488b14213d
bdushimi <lespharaons@hotmail.com>
2022-03-15 23:37:51 +0200

    Configures js files to ES Modules

--- otp-manager/OTPController.js
+++ otp-manager/OTPController.js
@@ -1,9 +1,42 @@
 'use strict';
-import { sendResponse } from './utils/sendResponse';
+import { sendResponse } from './utils/sendResponse.js';
 
 export async function generate(event) {
 
-  const { phoneNumber } = JSON.parse(event.body);
+  const { expiry, message, mobile, sender_id } = JSON.parse(event.body);
 
-  return sendResponse(200, { message: 'Phone Number has been retrieved successfully', phoneNumber });
+  const options = {
+    method: 'POST',
+    url: 'https://d7-verify.p.rapidapi.com/send',
+    headers: {
+      'content-type': 'application/json',
+      authorization: 'undefined',
+      'x-rapidapi-host': 'd7-verify.p.rapidapi.com',
+      'x-rapidapi-key': '0894320c95msh9b0466dd3573520p171759jsn82db79091d5c'
+    },
+    data: {
+      expiry,
+      message,
+      mobile,
+      sender_id
+    }
+  };
+
+  axios.request(options).then(function (response) {
+    return sendResponse(
+      200,
+      {
+        message: 'OTP generated successfully',
+        data: response.data
+      }
+    );
+  }).catch(function (error) {
+    return sendResponse(
+      500,
+      {
+        message: 'Could not generate OTP',
+        error
+      }
+    );
+  });
 }
