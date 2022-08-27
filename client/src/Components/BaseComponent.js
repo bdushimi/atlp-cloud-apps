1f4a7e38934bc7d3c01b525e2945098c20e323de
bdushimi <lespharaons@hotmail.com>
2022-08-27 15:33:54 +0200

    Setup frontend project

---
+++ client/src/Components/BaseComponent.js
@@ -0,0 +1,61 @@
+// Add your imports here
+import { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import SearchCourse from "./SearchCourse";
+import Courses from "./Courses";
+import axios from "axios";
+
+const BaseComponent = ({ api }) => {
+    const [state, setState] = useState({
+        courses: [],
+        search_string: "",
+        loaded: false,
+    });
+
+    const fetchData = async () => {
+        try {
+            const endpoint = api + '/fetchAllCourses';
+            const res = await axios.get(endpoint);
+            if (res.data.length > 0) {
+                setState((prevState) => {
+                    return { ...prevState, courses: res.data, loaded: true };
+                });
+            }
+            else
+                setState((prevState) => {
+                    return { ...prevState, loaded: true };
+                });
+        }
+        catch (error) {
+            console.log("Error : " + error);
+            setState((prevState) => {
+                return { ...prevState, loaded: true };
+            });
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const handleSearchStringUpdate = (searchString) => {
+        setState((prevState) => {
+            return { ...prevState, search_string: searchString };
+        });
+    };
+
+    return (
+        <Container>
+            <SearchCourse
+                search_string={state.search_string}
+                searchStringUpdated={handleSearchStringUpdate}
+            />
+            <Courses
+                courses_data={state.courses}
+                loaded_from_db={state.loaded}
+                search_string={state.search_string}
+            />
+        </Container>
+    );
+}
+export default BaseComponent;
